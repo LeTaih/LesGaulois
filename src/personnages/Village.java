ef304e3e210847bb454adc19e2e2c2c8d570e582
GRT4785A <GRT4785A@u3-210-02.ens.fsi>
2023-03-07 09:43:21 +0100

    tp2: Village++ presque fonctionnel

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,14 +3,14 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
-	private String[] villageois;
+	private Gaulois[] villageois;
 	private int nbVillageois;
 	private int nbVillageoisMaximum;
 	
-	public Village(String nom, Chef chef, String[] villageois, int nbVillageois) {
+	public Village(String nom, Chef chef, Gaulois[] villageois, int nbVillageois) {
 		this.nom = nom;
 		this.chef = chef;
-		villageois = new String[nbVillageoisMaximum];
+		villageois = new Gaulois[nbVillageoisMaximum];
 		nbVillageois = 0;
 	}
 	
@@ -21,25 +21,25 @@ public class Village {
 		this.chef = chef;
 	}
 	
-	public String ajouterHabitant(Gaulois gaulois) {
-		villageois[nbVillageois] = gaulois.getNom();
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
 		nbVillageois++;
 		
 	}
-	private int nb;
-	public int trouverHabitant(int nb) {
+	
+	public Gaulois trouverHabitant(int nb) {
 		return villageois[nb];
 	}
 	
 	public static void main(String[] args) {
 		Village village;
 		Chef chef;
-		String villageois[];
-		village = new Village("Village des Irréductibles", chef, villageois[], 0);
+		Gaulois villageois[];
+		village = new Village("Village des Irréductibles", chef, villageois, 0);
 		
 		System.out.println(village.getNom());
 		Gaulois gaulois = village.trouverHabitant(30);
-		}
+		
 	
 	}
 	
