a9e64156fb6b9dfa43a912d0f3a67ada8f34ee3d
Thomas Gerouit <grt4785a@ens.fsi>
2023-09-29 09:46:00 +0200

    TP2.1.a(2)

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,10 +3,12 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	
 	public Romain(String nom, int force) {
-		super();
 		this.nom = nom;
 		this.force = force;
+		assert force >=0 : "La force d'un romain doit être positive.";
+		
 	}
 	
 	public String getNom() {
@@ -33,7 +35,7 @@ public class Romain {
 		return "Romain [nom=" + nom + ", force=" + force + "]";
 	}
 	public static void main(String[] args) {
-		Romain caesar = new Romain("Caesar",7);
+		Romain caesar = new Romain("Caesar",-3);
 		Gaulois asterix = new Gaulois("Astérix",8);
 		System.out.println(asterix);
 		caesar.parler("Hello");
